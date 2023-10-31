e21004720285800adad8368d34ee656726fc9bcd
Manuel <manolo.meniba@gmail.com>
2023-10-31 14:11:25 +0100

    build grande

---
+++ make_version.js
@@ -0,0 +1,14 @@
+const PACKAGE_VERSION = require("./package.json").version;
+const chalk = require('chalk');
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
+console.log(chalk.blue('Versión de la aplicación'));
+console.error('     ' + PACKAGE_VERSION);
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
