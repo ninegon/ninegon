b34f84b9f26e419e8ac6229d96d5f9e18148b51a
Manuel <manolo.meniba@gmail.com>
2023-10-31 14:41:19 +0100

    build terminado

--- make_version.js
+++ make_version.js
@@ -1,14 +1,8 @@
 const PACKAGE_VERSION = require("./package.json").version;
 const chalk = require('chalk');
-console.log()
-console.log()
-console.log()
-console.log()
+console.log(chalk.blue('---------------------------------------'))
 console.log()
 console.log(chalk.blue('Versión de la aplicación'));
 console.error('     ' + PACKAGE_VERSION);
 console.log()
-console.log()
-console.log()
-console.log()
-console.log()
+console.log(chalk.blue('---------------------------------------'))
